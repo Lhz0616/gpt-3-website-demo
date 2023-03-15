860f169830c3f9cc2c108eb0465ad33f32c80945
Lim Hong Zhi <lim1234172@gmail.com>
2023-03-15 20:06:43 +0800

    Added assests

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,19 @@
 import react from 'react';
+import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
 import './navbar.css';
+
+import logo from "../../assets/logo.svg";
+
+// BEM -> Block Element Modifier (Naming convention for className)
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
+        <div className="gpt3__navbar">
+            <div className="gpt3__navbar-links">
+                <div className="gpt__3navbar-links_logo">
+                    <img src={logo} alt="logo" />  
+                </div>
+            </div>
         </div>
         )
 }
