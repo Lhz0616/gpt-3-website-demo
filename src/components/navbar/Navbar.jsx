30bae12fadc0b9716c8d393f584dbc418feb96f2
Lim Hong Zhi <lim1234172@gmail.com>
2023-03-15 20:43:21 +0800

    update navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import react from 'react';
 import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
 import './navbar.css';
 
-import logo from "../assets/logo.svg";
+import logo from "../../assets/logo.svg";
 
 // BEM -> Block Element Modifier (Naming convention for className)
 
@@ -10,8 +10,21 @@ const Navbar = () => {
     return (
         <div className="gpt3__navbar">
             <div className="gpt3__navbar-links">
-                <div className="gpt__3navbar-links_logo">
+                <div className="gpt3__navbar-links_logo">
                     <img src={logo} alt="logo" />  
+                </div>
+
+                <div className="gpt3__navbar-links_container">
+                    <p><a href="#home">Home</a></p>
+                    <p><a href="#wgpt3">What is GPT3</a></p>
+                    <p><a href="#possibility">Open AI</a></p>
+                    <p><a href="#feature">Case Studies</a></p>
+                    <p><a href="#blog">Library</a></p>
+                </div>
+
+                <div className="gpt3__navbar-sign">
+                    <p>Sign In</p>
+                    <button type="button">Sign Up</button>
                 </div>
             </div>
         </div>
