9fe40db6d1e4fac07a423962888adb167ba9b570
Lim Hong Zhi <lim1234172@gmail.com>
2023-03-16 21:27:58 +0800

    finish navigation bar, move to header now

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,8 +3,17 @@ import './header.css';
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className="gpt3__header section__padding" id="home">
+            <div className="gpt3__header-content">
+                <h1 className="gradient__text">Let's Build Something amazing with GPT-3 OpenAI</h1>
+                <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+
+                <div className="gpt3__header-content__input">
+                    <input type="email" placeholder="Your email address..."/>
+                    <button type="button">Get started</button>
+                
+                </div>
+            </div>
         </div>
         )
 }
